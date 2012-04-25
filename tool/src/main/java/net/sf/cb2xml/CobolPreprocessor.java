e9b6b9cbfb06319c4e83844ddfdf7853a4d08e92
miolini <miolini@gmail.com>
2012-04-25 10:54:23 -0400

    fixing preprocessing

--- tool/src/main/java/net/sf/cb2xml/CobolPreprocessor.java
+++ tool/src/main/java/net/sf/cb2xml/CobolPreprocessor.java
@@ -8,80 +8,81 @@
 
 package net.sf.cb2xml;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Properties;
 
 /**
- * Very simple COBOL pre-processor that chops the left and right margins. 
- * Column start and end positions are configurable using a properties file. 
+ * Very simple COBOL pre-processor that chops the left and right margins.
+ * Column start and end positions are configurable using a properties file.
  * Linefeeds are retained as these are required by the main parser.
- * COBOL files typically contain some junk characters and 
- * comment indicators in the "margins" and this routine removes those. 
- * 
+ * COBOL files typically contain some junk characters and
+ * comment indicators in the "margins" and this routine removes those.
+ *
  * @author Peter Thomas
  */
 public class CobolPreprocessor {
 
-	  public static String preProcess(File file) {
-	  	int columnStart = 6;
-	  	int columnEnd = 72;
-	  	try {
-	  	  	FileInputStream propsStream = new FileInputStream("cb2xml.properties");
-		  	Properties props = new Properties();
-		  	props.load(propsStream);
-		  	propsStream.close();
-		  	String columnStartProperty = props.getProperty("column.start");
-		  	String columnEndProperty = props.getProperty("column.end");
-		  	if (columnStartProperty != null) {
-		  		columnStart = Integer.parseInt(columnStartProperty);
-		  	}
-		  	if (columnEndProperty != null) {
-		  		columnEnd = Integer.parseInt(columnEndProperty);
-		  	}
-	  	} catch (Exception e) {
-	  		e.printStackTrace();
-	  	}
-	  	System.err.println("*** using start column = " + columnStart + ", end column = " + columnEnd);
-	    FileInputStream fis = null;
-	    BufferedReader buffer = null;
-	    StringBuffer sb = new StringBuffer();
-	    String s = null;
-	    int startPosition;
-	    try {
-	      fis = new FileInputStream(file);
-	      buffer = new BufferedReader(new InputStreamReader(fis));
-	      while ((s = buffer.readLine()) != null) {	      	
-	      	if (s.length() > columnStart) {
-	      		int thisColumnStart = columnStart;
-		      	if (s.charAt(columnStart) == '/') {
-		      		sb.append('*');
-		      		thisColumnStart++;
-		      	}	      		
-	      		if (s.length() < columnEnd) {
-	      			sb.append(s.substring(thisColumnStart));
-	      		} else {
-	      			sb.append(s.substring(thisColumnStart, columnEnd));
-	      		}  	
-	      	}
-	        sb.append("\n");
-	      }
-	    }
-	    catch (Exception e) {
-	      e.printStackTrace();
-	      return null;
-	    }
-	    finally {
-	      if (fis != null) {
-	        try {
-	          fis.close();
-	        } catch (IOException e) {}
-	      }
-	    }
-	    return sb.toString();
-	  }
+	public static String preProcess(File file) {
+		int columnStart = 6;
+		int columnEnd = 72;
+		try {
+			FileInputStream propsStream = new FileInputStream("cb2xml.properties");
+			Properties props = new Properties();
+			props.load(propsStream);
+			propsStream.close();
+			String columnStartProperty = props.getProperty("column.start");
+			String columnEndProperty = props.getProperty("column.end");
+			if (columnStartProperty != null) {
+				columnStart = Integer.parseInt(columnStartProperty);
+			}
+			if (columnEndProperty != null) {
+				columnEnd = Integer.parseInt(columnEndProperty);
+			}
+		} catch (FileNotFoundException fe) {
+			System.out.println("properties file for preprocessor not found");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.err.println("*** using start column = " + columnStart + ", end column = " + columnEnd);
+		FileInputStream fis = null;
+		BufferedReader buffer = null;
+		StringBuffer sb = new StringBuffer();
+		String s = null;
+		int startPosition;
+		try {
+			fis = new FileInputStream(file);
+			buffer = new BufferedReader(new InputStreamReader(fis));
+			while ((s = buffer.readLine()) != null) {
+				if (!s.trim().startsWith("*")) {
+					s = s.replaceAll(":", "");
+					s = s.replaceAll("^(.+?)([\\d]+)$", "$1");
+				}
+				if (s.length() > columnStart) {
+					int thisColumnStart = columnStart;
+					if (s.charAt(columnStart) == '/') {
+						sb.append('*');
+						thisColumnStart++;
+					}
+					if (s.length() < columnEnd) {
+						sb.append(s.substring(thisColumnStart));
+					} else {
+						sb.append(s.substring(thisColumnStart, columnEnd));
+					}
+				}
+				sb.append("\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return sb.toString();
+	}
 
 }
