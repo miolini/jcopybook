a16ef178dde8a14d482bc7a1bb258009d35258d3
miolini <miolini@gmail.com>
2012-04-25 10:58:06 -0400

    fixed null in text node after MainframeToXml

--- tool/src/main/java/net/sf/cb2xml/util/XmlUtils.java
+++ tool/src/main/java/net/sf/cb2xml/util/XmlUtils.java
@@ -8,8 +8,8 @@
 
 package net.sf.cb2xml.util;
 
-import java.io.File;
-import java.io.StringWriter;
+import org.jcopybook.Utils;
+import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
@@ -19,86 +19,87 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
-import org.w3c.dom.Document;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.StringWriter;
 
 /**
-* quick and easy XML utilities to ease the trouble of DOM parsing, XSL transformations
-* or converting XML to Strings / StringBuffer and vice-versa
-* @author Peter Thomas
-*/
+ * quick and easy XML utilities to ease the trouble of DOM parsing, XSL transformations
+ * or converting XML to Strings / StringBuffer and vice-versa
+ *
+ * @author Peter Thomas
+ */
 
 public class XmlUtils {
 
-  public static Document getNewXmlDocument() {
-    try {
-      return DocumentBuilderFactory.newInstance().
-          newDocumentBuilder().newDocument();
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
+	public static Document getNewXmlDocument() {
+		try {
+			return DocumentBuilderFactory.newInstance().
+					newDocumentBuilder().newDocument();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-  public static StringBuffer domToString(Document document) {
-  	if (document == null) {
-  		return new StringBuffer();
-  	}
-    return transformToString(new DOMSource(document), null);
-  }
+	public static StringBuffer domToString(Document document) {
+		if (document == null) {
+			return new StringBuffer();
+		}
+		// Fix null in text nodes
+		Utils.fixStringNode(document);
+		return transformToString(new DOMSource(document), null);
+	}
 
-  public static Document fileToDom(String fileName) {
-    try {
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      return factory.newDocumentBuilder().parse(new File(fileName));
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
+	public static Document fileToDom(String fileName) {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(fileName);
+			if (stream == null) throw new FileNotFoundException("resources not found: "+fileName);
+			return factory.newDocumentBuilder().parse(stream);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-  public static StringBuffer transformToString(Source xmlSource,
-                                               Source xslSource) {
-    StringWriter writer = new StringWriter();
-    Transformer transformer;
-    try {
-      if (xslSource == null) {
-        transformer = TransformerFactory.newInstance().newTransformer();
-      }
-      else {
-        transformer = TransformerFactory.newInstance().newTransformer(xslSource);
-      }
-      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-      transformer.transform(xmlSource, new StreamResult(writer));
-      return writer.getBuffer();
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      return writer.getBuffer();
-    }
-    finally {
-      try {
-        writer.close();
-      }
-      catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
+	public static StringBuffer transformToString(Source xmlSource,
+												 Source xslSource) {
+		StringWriter writer = new StringWriter();
+		Transformer transformer;
+		try {
+			if (xslSource == null) {
+				transformer = TransformerFactory.newInstance().newTransformer();
+			} else {
+				transformer = TransformerFactory.newInstance().newTransformer(xslSource);
+			}
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.transform(xmlSource, new StreamResult(writer));
+			return writer.getBuffer();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return writer.getBuffer();
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  public static StringBuffer transformToString(String xmlFileName,
-                                               String xslFileName) {
-    return transformToString(new StreamSource(new File(xmlFileName)),
-                             new StreamSource(new File(xslFileName)));
-  }
+	public static StringBuffer transformToString(String xmlFileName,
+												 String xslFileName) {
+		return transformToString(new StreamSource(new File(xmlFileName)),
+				new StreamSource(new File(xslFileName)));
+	}
 
-  public static StringBuffer transformToString(Document document,
-                                               String xslFileName) {
-    return transformToString(new DOMSource(document),
-                             new StreamSource(new File(xslFileName)));
-  }
+	public static StringBuffer transformToString(Document document,
+												 String xslFileName) {
+		return transformToString(new DOMSource(document),
+				new StreamSource(new File(xslFileName)));
+	}
 
 
 }
--- tool/src/main/java/org/jcopybook/Utils.java
+++ tool/src/main/java/org/jcopybook/Utils.java
@@ -1,6 +1,7 @@
 package org.jcopybook;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Utils {
@@ -13,5 +14,19 @@ public class Utils {
 			if (node instanceof Element) return (Element) node;
 		}
 		throw new RuntimeException("Can't find any Element");
+	}
+
+
+	/**
+	 * Iterative fix null value in text node
+	 * @param node
+	 */
+	public static void fixStringNode(Node node) {
+		if (node.getNodeType() == Node.TEXT_NODE && node.getNodeValue() == null)
+			node.setNodeValue("");
+		NodeList childs = node.getChildNodes();
+		for (int i = 0; i < childs.getLength(); i++) {
+			fixStringNode(childs.item(i));
+		}
 	}
 }
